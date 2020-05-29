99a42a5a146c4c6a288b073fdb4554fbe9033040
luis moreno <teckforever@gmail.com>
2020-05-29 14:17:05 -0500

    Se agrega plugin de swiper.js

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,11 +1,11 @@
 
-
 //#region ELEMENTOS DEL DOM
 
 let nombreMascota = document.getElementById('txt_nombreMascota')
 let edadMascota = document.getElementById('txt_edadMascota')
 let btnFormulario = document.getElementById('btnFormulario')
-
+let textoHeader = document.getElementById('textoHeader')
+let formularioMascota = document.getElementById('formularioMascota')
 
 //#endregion ELEMENTOS DEL DOM
 
@@ -26,7 +26,7 @@ abrirMensaje = (mensajeModal, tipoModal) => {
             modal = 'mensaje-info'
         } else if (tipoModal == 'Exito') {
             modal = 'mensaje-exitoso'
-            removerClase='mensaje-error'
+            removerClase = 'mensaje-error'
         } else if (tipoModal == 'Error') {
             modal = 'mensaje-error'
             removerClase = 'mensaje-exitoso'
@@ -87,17 +87,28 @@ validarFormulario = () => {
 
 }
 
-btnFormulario.addEventListener('click', (e) => {
-    e.preventDefault()
-    if (validarFormulario()) {
-        abrirMensaje('Guardado exitosamente','Exito')
-    }else{
-        abrirMensaje('Recuerda llenar los campos','Error')
-    }
-})
-
 //#endregion
 
+//#region GUARDAR DATOS LOCALSTORAGE
+
+guardarMascotaLocalStorage = (nuevaMascota) => {
+
+    /* Recordar modificarlo ya que viene como string */
+    let mascotasLocalStorage = JSON.parse(localStorage.getItem('mascotasGuardadas'))
+
+    console.log("Base de datos")
+    console.log(mascotasLocalStorage)
+
+    mascotasLocalStorage.push(nuevaMascota)
+
+    console.log("Nueva base de datos")
+    console.log(mascotasLocalStorage)
+
+    localStorage.setItem('mascotasGuardadas',JSON.stringify(mascotasLocalStorage))
+
+}
+
+//#endregion GUARDAR DATOS LOCALSTORAGE
 
 //#region DOMCONTENTLOADED
 
@@ -105,13 +116,55 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let mensajeModal = 'Bienvenido a Mascotas'
     let tipoModal = 'Exito' /* Info Exito Error */
+    let mascotasGuardadas = []
+    let validarBaseDeDatos = JSON.parse (localStorage.getItem('mascotasGuardadas'))
 
     abrirMensaje(mensajeModal, tipoModal)
+
+    !validarBaseDeDatos ? localStorage.setItem('mascotasGuardadas', JSON.stringify(mascotasGuardadas)) : null
+
+    console.log("Base de datos Mascotas")
+    console.log(validarBaseDeDatos)
 
 })
 
 //#endregion DOMCONTENTLOADED
 
+//#region EVENTOS DEL DOM
+
+//GUARDAR DATOS
+
+btnFormulario.addEventListener('click', (e) => {
+    e.preventDefault()
+    if (validarFormulario()) {
+        abrirMensaje('Guardado exitosamente', 'Exito')
+
+        let nuevaMascota = {
+            nombre: nombreMascota.value,
+            edad: edadMascota.value
+        }
+
+        guardarMascotaLocalStorage(nuevaMascota);
+
+        formularioMascota.reset()
+
+        /* Revisar como volver todos los datos de un objeto en false */
+
+        itemsFormulario.valNombreMascota = false
+        itemsFormulario.valEdadMascota = false
+
+    } else {
+        abrirMensaje('Recuerda llenar los campos', 'Error')
+    }
+})
+
+
+textoHeader.addEventListener('click', (e) => {
+    window.location.href = './mascotas.html'
+})
+
+//#endregion
 
 
 
+/* Reto personal cargar imagen desde el ordenador para guardar en una carpeta de la pagina y despues de esto asignarla a la pagina aunque no es recomendable ya que la pagina pesaria  mucho mejor guardarle en una base de datos para extraerla y mostrarla como la de firebase :D depronto como precarga para subirla al servidor*/
